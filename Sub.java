5982ad48f6d3f28cc9d6f6cc14dd3afb63bff753
pradeepkumar2060 <s.pradeep2060>
2018-07-06 08:28:30 +0000

    created Sub.java file..

---
+++ Sub.java
@@ -0,0 +1,17 @@
+//addtion of two numbers
+
+import java.io.*;
+class Sub{
+public static void main(String [] args)
+{
+int a =10;
+int b = 20;
+int sub = (b-a);
+System.out.println("Subtraction is: "+sub);
+}
+
+}
+
+
+
+
