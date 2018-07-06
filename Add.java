a9beac0fbc019d6bec8dfb4235ff895ddb3e6d2c
pradeepkumar2060 <s.pradeep2060>
2018-07-06 07:41:54 +0000

    created jav file for addtion of to numbers

---
+++ Add.java
@@ -0,0 +1,17 @@
+//addtion of two numbers
+
+import java.io.*;
+class Add{
+public static void main(String [] args)
+{
+int a =10;
+int b = 20;
+int add = (a+b);
+System.out.println("addition of number is: "+add);
+}
+
+}
+
+
+
+
